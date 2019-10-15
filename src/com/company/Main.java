c7c83055f7a20cdac622f51c8560d70ff77a57a7
Ziggy <123>
2019-10-15 18:08:22 +0800

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.io.*;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+	// write your code here
+        DGIM dgim = new DGIM(1000);
+        try(BufferedReader reader =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    new FileInputStream(
+                                            "cmsc5741_stream_data1.txt")))) {
+            while(true){
+                int c = reader.read();
+                if(c==-1){
+                    break;
+                }
+                DGIM.number++;
+                if((char)c=='1'){
+                    dgim.add();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Iterator<Bucket> iterator = dgim.getQueue().iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println(dgim.getContent());
+    }
+}
